cd9e76ffe0d94d5b36605a10e5edaa9169a895d0
BUCHU <710625937@qq.com>
2019-07-29 16:42:53 +0800

    https://github.com/huran111/spring-boot-dubbo.git

---
+++ spring-boot-dubbo-api/src/main/java/com/hr/dubbo/server/user/api/UserService.java
@@ -0,0 +1,11 @@
+package com.hr.dubbo.server.user.api;
+
+/**
+ * @author 胡冉
+ * @ClassName UseService
+ * @Date 2019/7/29 10:42
+ * @Version 2.0
+ */
+public interface UserService {
+    String sayHello(String message);
+}
